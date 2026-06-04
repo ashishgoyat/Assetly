3e7e210a2aff723f0eb9b499835a5ecfd1fee4ab
Ashish Goyat <ashishgoyat.official@gmail.com>
2026-06-04 11:45:14 +0530

    fix: disable prepared statements for Supabase PgBouncer transaction pool

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,7 +2,7 @@ import postgres from 'postgres'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import * as schema from './schema'
 
-const client = postgres(process.env.DATABASE_URL!, { max: 1 })
+const client = postgres(process.env.DATABASE_URL!, { max: 1, prepare: false })
 
 export const db = drizzle(client, { schema })
 
